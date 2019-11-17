1774270c7e89e3cbc4d51c2b4c75e57c3b5eb72e
andong <ad@andongdeMacBook-Air.local>
2019-11-17 15:09:44 +0800

    init repos

---
+++ src/main/java/com/majiang/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
